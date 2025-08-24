102aeff25c9330525fec8d4561ba4472e5edf67a
jmessk <jmerusty@gmail.com>
2025-08-24 12:28:11 +0100

    boot.ipxe

--- http-server/files/os/ubuntu/24.04.3/server/amd64/fetch-images.bash
+++ http-server/files/os/ubuntu/24.04.3/server/amd64/fetch-images.bash
@@ -7,10 +7,12 @@ cd "$(dirname -- "${BASH_SOURCE[0]}")"
 
 need() {
 	local file="$1" url="$2"
+
 	if [[ -f $file ]]; then
 		echo "Already exists, skipping: $file"
 		return
 	fi
+
     echo "Downloading ${url} -> ${file}" >&2
 	curl -L --fail -o "$file.tmp" "$url"
 	mv "$file.tmp" "$file"
